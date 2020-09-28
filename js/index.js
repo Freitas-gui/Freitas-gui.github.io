ac05fe6b13cc8fd7989cc33967207cd570c1c2fe
Guilheme Freitas <freitas-gui19@hotmail.com>
2020-09-28 12:55:21 -0300

    menu sticky jquery

--- js/index.js
+++ js/index.js
@@ -1,3 +1,69 @@
+/*start menu nav scroll*/
+var lastScrollTop = 0;
+
+window.addEventListener('scroll', function (e) {
+    if ($(window).width() <= 450) {
+        var jElement = $('.main-menu');
+        // mesma posição
+        if (e.scrollY === lastScrollTop) return;
+
+        if (this.scrollY < lastScrollTop) {
+            jElement.css({
+                'position': 'fixed',
+                'top': '0px'
+            });
+        } else {
+            jElement.css({
+                'position': 'relative',
+                'top': 'auto'
+            });
+        }
+
+
+        lastScrollTop = this.scrollY;
+    }
+
+  }, true)
+
+
+var body = document.getElementsByTagName("body")[0];
+
+if (body.addEventListener) {
+    // IE9, Chrome, Safari, Opera
+    body.addEventListener("mousewheel", detectarDirecaoRolagem, false);
+    // Firefox
+    body.addEventListener("DOMMouseScroll", detectarDirecaoRolagem, false);
+}
+
+function detectarDirecaoRolagem( e )
+{
+    var jElement = $('.main-menu');
+    var delta = null,
+        direction = false
+    ;
+    if ( !e ) {
+        e = window.event;
+    }
+    if ( e.wheelDelta ) { // funciona na maioria dos casos
+        delta = e.wheelDelta / 60;
+    } else if ( e.detail ) { // funciona no Firefox
+        delta = -e.detail / 2;
+    }
+    if ( delta !== null ) {
+        direction = delta > 0 ? 'cima' : 'baixo';
+    }
+
+    console.log(direction);
+
+    return direction;
+}
+
+/*end menu nav scroll*/
+
+
+
+/*start Responsive*/
+
 function reloadPage() {
     /* When window is smaller than 990, then DOM vanhish, else then, show DOM.  */
     jQuery(document).ready(function () {
@@ -30,3 +96,5 @@ window.onresize = function(){
 
 /*Caled reloadPage() when update page*/
 jQuery(document).ready(reloadPage() );
+
+/*end Responsive*/
