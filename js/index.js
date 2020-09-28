6bbc7fe41d53422c0c6dbc91fb47d02ec1590105
Guilheme Freitas <freitas-gui19@hotmail.com>
2020-09-28 15:46:06 -0300

    center menu-nav

--- js/index.js
+++ js/index.js
@@ -1,4 +1,48 @@
+/*start Responsive*/
+
+function reloadPage() {
+    /* When window is smaller than 990, then DOM vanhish, else then, show DOM.  */
+    jQuery(document).ready(function () {
+        if ($(window).width() <= 990) {
+            $(".hide-show").hide();
+        } else {
+            $(".hide-show").show();
+        }
+    });
+    jQuery(document).ready(function () {
+        /* When window is smaller than 990, and .container isn't parent of #carouselExampleControls-media then the parent destroyed.
+        *   else then, # carouselExampleControls-media will be involved for carouselExampleControls.
+        * */
+        if ($(window).width() <= 990) {
+            if(!($("#carouselExampleControls-media").parent().is($(".container"))))
+                $("#carouselExampleControls-media").unwrap();
+        }
+        else if ($("#carouselExampleControls-media").parent().is($(".container"))){
+            $("#carouselExampleControls-media").wrap("<div id=\"carouselExampleControls\" class=\"carousel slide\" data-ride=\"carousel\"></div>");
+        }
+    });
+}
+
+/* Called reloadPage() every 200 mls if during this, window change size. */
+var run_responsive;
+window.onresize = function(){
+  clearTimeout(run_responsive);
+  run_responsive = setTimeout(reloadPage, 200);
+};
+
+/*Caled reloadPage() when update page*/
+jQuery(document).ready(reloadPage() );
+
+/*end Responsive*/
+
+
+
+
+
+
+
 /*start menu nav scroll*/
+/*
 var lastScrollTop = 0;
 
 window.addEventListener('scroll', function (e) {
@@ -57,44 +101,8 @@ function detectarDirecaoRolagem( e )
 
     return direction;
 }
-
+*/
 /*end menu nav scroll*/
 
 
 
-/*start Responsive*/
-
-function reloadPage() {
-    /* When window is smaller than 990, then DOM vanhish, else then, show DOM.  */
-    jQuery(document).ready(function () {
-        if ($(window).width() <= 990) {
-            $(".hide-show").hide();
-        } else {
-            $(".hide-show").show();
-        }
-    });
-    jQuery(document).ready(function () {
-        /* When window is smaller than 990, and .container isn't parent of #carouselExampleControls-media then the parent destroyed.
-        *   else then, # carouselExampleControls-media will be involved for carouselExampleControls.
-        * */
-        if ($(window).width() <= 990) {
-            if(!($("#carouselExampleControls-media").parent().is($(".container"))))
-                $("#carouselExampleControls-media").unwrap();
-        }
-        else if ($("#carouselExampleControls-media").parent().is($(".container"))){
-            $("#carouselExampleControls-media").wrap("<div id=\"carouselExampleControls\" class=\"carousel slide\" data-ride=\"carousel\"></div>");
-        }
-    });
-}
-
-/* Called reloadPage() every 200 mls if during this, window change size. */
-var run_responsive;
-window.onresize = function(){
-  clearTimeout(run_responsive);
-  run_responsive = setTimeout(reloadPage, 200);
-};
-
-/*Caled reloadPage() when update page*/
-jQuery(document).ready(reloadPage() );
-
-/*end Responsive*/
